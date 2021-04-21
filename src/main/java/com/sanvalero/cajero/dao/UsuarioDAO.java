ef78b29fac91a0b0ad6b36c11bad1e437d2c85e9
Jorge F <jorgefuli91@gmail.com>
2021-04-21 13:21:43 +0100

    v0.2 Creados gestores de usuarios y cuentascorrientes

--- src/main/java/com/sanvalero/cajero/dao/UsuarioDAO.java
+++ src/main/java/com/sanvalero/cajero/dao/UsuarioDAO.java
@@ -13,17 +13,20 @@ import java.util.ArrayList;
 public class UsuarioDAO {
 
     private Conexion connection;
+
     public UsuarioDAO(Conexion connection) {
         this.connection = connection;
     }
-/**
+
+    /**
      * Registra nuevo usuario y lo añade a la bbdd.
-     * @param user
-     * @throws SQLException 
+     *
+     * @param usuario
+     * @throws SQLException
      */
     public void registroUsuario(Usuario usuario) throws SQLException {
-        String sql = "INSERT INTO USUARIO (NOMBRE, APELLIDOS, DNI, EMAIL, TELEFONO, CONTRASENA) " +
-                "VALUES (?, ?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO USUARIO (NOMBRE, APELLIDOS, DNI, EMAIL, TELEFONO, CONTRASENA) "
+                + "VALUES (?, ?, ?, ?, ?, ?)";
 
         PreparedStatement query = connection.getConexion().prepareStatement(sql);
 
@@ -35,30 +38,64 @@ public class UsuarioDAO {
         query.setString(6, usuario.getContrasena());
         query.executeUpdate();
     }
-    //TODO para juntar usuario con cuenta
+    //Para envíar una sentencia SQL del tipo Consulta se utliza el método executeQuery(sql);
+    //Para update executeUpdate(); 
+
+    /*
+     * Determina el id_usuario pidiendo al usuario su nombre y apellidos
+     */
+    public void verId(String dni, String contrasena) throws SQLException {
+        String sql = "SELECT id_usuario FROM USUARIO WHERE dni = ? AND  contrasena = ?)";
+        PreparedStatement sentencia = connection.getConexion().prepareStatement(sql);
+        sentencia.setString(1, dni);
+        sentencia.setString(2, contrasena);
+        ResultSet resultado = sentencia.executeQuery(sql);
+        resultado.getInt("id_usuario");
+    }
+        
+  
+        //    public Usuario showId(String nombre, String apellidos) throws SQLException {
+        //        String sql = "SELECT id_usuario FROM USUARIO WHERE nombre = ? AND  apellidos = ?)";
+        //        
+        //        PreparedStatement sentencia = connection.getConexion().prepareStatement(sql);
+        //        sentencia.setString(1, nombre);
+        //        sentencia.setString(2, apellidos);
+        //        ResultSet resultado = sentencia.executeQuery(sql);  //executeQuery
+        //        resultado.getInt("id_usuario");
+        //        return getString(resultado.parseInt.toInt());
+        ////        while (resultado.next()) {
+        ////        int id_usuario = resultado.getInt("id_usuario");
+        ////        }
+        //        
+        //        //return Usuario.sentencia.getInt("id_usuario");
+        //        //return showId(nombre, apellidos);
+        //    }
+        
+        
+        //TODO para juntar usuario con cuenta
     public ArrayList<Usuario> obtenerUsuario(String cadenaBusqueda) throws SQLException {
         String sql = "SELECT * FROM USUARIO WHERE DNI = ?";
         ArrayList<Usuario> usuario1 = new ArrayList<>();
-        
+
         PreparedStatement sentencia = connection.getConexion().prepareStatement(sql);
         sentencia.setString(1, cadenaBusqueda);
         ResultSet resultado = sentencia.executeQuery();
-        while (resultado.next()) {          
+        while (resultado.next()) {
             Usuario usuario = new Usuario();
             usuario.setDni(resultado.getString(3));
-            
+
             usuario1.add(usuario);
-        }        
+        }
         return usuario1;
     }
 //TODO
+
     public void eliminarUsuario(String dni, String contrasena) throws SQLException {
-        String sql = "DELETE FROM USUARIO WHERE DNI = ?";
-       
+        String sql = "DELETE FROM USUARIO WHERE DNI = ? AND  contrasena = ?)";
+
         PreparedStatement sentencia = connection.getConexion().prepareStatement(sql);
-        sentencia.setString(1, matricula);
+        sentencia.setString(1, dni);
+        sentencia.setString(2, contrasena);
         sentencia.executeUpdate();
     }
 }
-
-
